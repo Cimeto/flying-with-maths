c922e27599d2f41f4d63f11ea0d5b5c197917e5a
Cimeto <84658014+Cimeto@users.noreply.github.com>
2021-05-24 11:11:36 +0200

    Added auxiliary enums and classes for the QuestionGenerator script

---
+++ Scripts/Enums/Operators.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// List of operations allowed in question generation
+/// </summary>
+public enum Operators
+{
+    Add,
+    Substract,
+    Multiply,
+    Divide
+}
---
+++ Scripts/Model/QuestionLevel.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// Level data class to use with the question gnerator.
+/// Minimum and Maximum for random number range and number of decimals allowed.
+/// </summary>
+public class QuestionLevel
+{
+    public int numberDecimals;
+    public int minimumRandom;
+    public int maximumRandom;
+}
