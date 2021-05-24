497a3aa884122962dbb499fcdd134655c5b1a079
Cimeto <84658014+Cimeto@users.noreply.github.com>
2021-05-24 12:22:13 +0200

    Added PrefabSpawners
    Added scripts used in FlyingWithMaths to spawn prefabs in game, defining spawning patterns (objects, timing, delay, etc)

---
+++ Scripts/Monobehaviours/PrefabSpawner.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Script to instantiate prefabs continuously in a 3D environment
+/// </summary>
+public class PrefabSpawner : MonoBehaviour
+{
+    public PrefabCollection prefabCollection;
+    public Transform[] spawnPoints;
+
+    public Vector3 positionRandomness;
+    public Vector3 rotationRandomness;
+    public float speed = 1;
+    public float spawnRate = 1;
+    public float prefabLifeTime = 10;
+    public float delay = 0;
+    public bool instantiateAtStart = true;
+    public bool attachToParent = true;
+
+    private float timer = 0;
+
+    private void Start()
+    {
+        if (instantiateAtStart)
+        {
+            timer = spawnRate; // to make it create one at the beginning
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Wait until delay time has been consumed.
+        if (delay > 0)
+        {
+            delay -= Time.deltaTime;
+            return;
+        }
+
+        // Count time to know when to spawn the next prefab
+        timer += Time.deltaTime;
+        if (timer >= spawnRate)
+        {
+            InstantiateRandomPrefab();
+            timer = 0;
+        }
+    }
+
+    // TODO I would refactor this in smaller chuncks
+    private void InstantiateRandomPrefab()
+    {
+        // Use this transform as spawnPoint
+        Transform spawnPoint = transform;
+
+        if (prefabCollection.prefabs.Length == 0)
+        {
+            return;
+        }
+
+        // Use a random spawnpoint if any
+        if (spawnPoints.Length > 0)
+        {
+            int randomSPIndex = Random.Range(0, spawnPoints.Length);
+            spawnPoint = spawnPoints[randomSPIndex];
+        }
+
+        // Calculate random position
+        Vector3 newPosition = GenerateRandomVector(spawnPoint.position, positionRandomness);
+
+        // Calculate random rotation
+        Quaternion newRotation = Quaternion.Euler(
+            GenerateRandomVector(spawnPoint.rotation.eulerAngles, rotationRandomness));
+
+        // Select a random prefab from the collection
+        int randomIndex = Random.Range(0, prefabCollection.prefabs.Length);
+        GameObject prefab = Instantiate(
+            prefabCollection.prefabs[randomIndex], newPosition, newRotation);
+
+        // Set speed and size
+        prefab.GetComponent<Rigidbody>().velocity = spawnPoint.forward * speed;
+
+        if (attachToParent)
+        {
+            prefab.transform.SetParent(spawnPoint);
+        }
+
+        // Set death time
+        Destroy(prefab, prefabLifeTime);
+    }
+
+    private Vector3 GenerateRandomVector(Vector3 origin, Vector3 randomness)
+    {
+        float randomX = Random.Range(origin.x - randomness.x, origin.x + randomness.x);
+        float randomY = Random.Range(origin.y - randomness.y, origin.y + randomness.y);
+        float randomZ = Random.Range(origin.z - randomness.z, origin.z + randomness.z);
+
+        return new Vector3(randomX, randomY, randomZ);
+    }
+}
---
+++ Scripts/Monobehaviours/PrefabSpawner2D.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Script to instantiate prefabs continuously in a 2D environment
+/// </summary>
+public class PrefabSpawner2D : MonoBehaviour
+{
+    public PrefabCollection prefabCollection;
+    public float prefabsSize = 1;
+    public Vector2 direction;
+    public float yRandomness = 0;
+    public float zRandomness = 0;
+    public float speed = 1;
+    public float spawnRate = 1;
+    public float spawnRateRandomness = 0;
+    public float prefabLifeTime = 5;
+    public bool instantiateAtStart = true;
+    public bool preCreatePrefabs = true;
+
+    private float timer = 0;
+    private float calculatedSpawnRate;
+
+    public void SetPrefabCollection(PrefabCollection prefabCollection)
+    {
+        this.prefabCollection = prefabCollection;
+
+        // TODO Using objects pool technique here implies updating the object pool content at this point
+    }
+
+    private void Start()
+    {
+        calculatedSpawnRate = Random.Range(spawnRate - spawnRateRandomness, spawnRate + spawnRateRandomness);
+
+        if (instantiateAtStart)
+        {
+            timer = calculatedSpawnRate; // to make it create one at the beginning
+        }
+
+        if (preCreatePrefabs)
+        {
+            PreInstantiateRandomPrefabs();
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Count time to know when to spawn the next prefab
+        timer += Time.deltaTime;
+        if (timer >= calculatedSpawnRate)
+        {
+            InstantiateRandomPrefab(transform.position.x, transform.position.y);
+            timer = 0;
+            calculatedSpawnRate = Random.Range(spawnRate - spawnRateRandomness, spawnRate + spawnRateRandomness);
+        }
+    }
+
+    // Create random prefabs in a line
+    private void PreInstantiateRandomPrefabs()
+    {
+        int quantity = (int)(prefabLifeTime / spawnRate);
+        float distancePerPrefab = spawnRate * speed;
+
+        while (quantity > 0)
+        {
+            InstantiateRandomPrefab(
+                transform.position.x + direction.x * distancePerPrefab * quantity,
+                transform.position.y + direction.y * distancePerPrefab * quantity);
+            quantity--;
+        }
+    }
+
+    private void InstantiateRandomPrefab(float x, float y)
+    {
+        if (prefabCollection.prefabs.Length == 0)
+        {
+            return;
+        }
+
+        // Calculating new position considering randomness of Z dimension
+        float randomY = Random.Range(transform.position.y - yRandomness, transform.position.y + zRandomness);
+        float randomZ = Random.Range(transform.position.z - zRandomness, transform.position.z + zRandomness);
+        Vector3 newPosition = new Vector3(x, randomY, randomZ);
+
+        // Random index in the prefab list and instantiating
+        int randomIndex = Random.Range(0, prefabCollection.prefabs.Length);
+
+        GameObject prefab =
+            Instantiate(prefabCollection.prefabs[randomIndex], newPosition, transform.rotation);
+        prefab.GetComponent<Rigidbody2D>().velocity = new Vector2(direction.x * speed, direction.y * speed);
+        prefab.transform.localScale = prefab.transform.localScale * prefabsSize;
+        prefab.transform.SetParent(transform);
+        Destroy(prefab, prefabLifeTime);
+
+        // IDEA. Set the prefab destroy in the instantiated prefab, triggered by distance from the parent. 
+        // (If it's too far, destroy it)
+    }
+}
